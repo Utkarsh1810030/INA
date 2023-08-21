d71cb8afa52bd7864328dba47ca4fb27b9d4d441
Utkarsh1810030 <uchaudhary1234@gmail.com>
2023-08-22 00:47:08 +0530

    keys removed

--- INA/config/prod.js
+++ INA/config/prod.js
@@ -1,10 +1,10 @@
 const { stripePublishableKey } = require("./dev");
 
 module.exports = {
-    googleClientID:process.env.GOOGLE_CLIENT_ID, //'182955185799-so1n6jgrmvdf786bi8vg308tfev9tnni.apps.googleusercontent.com'
-    googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,//'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'
-    mongoURI: process.env.MONGO_URI,//'mongodb+srv://uchaudhary1234:eoLWHbsolfghFGCf@cluster0.gbxyzn5.mongodb.net/prod?retryWrites=true&w=majority'
-    cookieKey:process.env.COOKIE_KEY,//'asdawdcascawde',
-    stripePublishableKey:process.env.STRIPE_PUBLISHABLE_KEY, //'pk_test_51NbjIGSFwQSeeZzUqw9VEk7JSIzlpLoku3B7Vikxf479tBZmjBl28cq95dh6NF29jU81CZG9XTrZ4OBjWditL3r500xp3d4ZGl'
-    stripeSecretKey:process.env.STRIPE_SECRET_KEY//sk_test_51NbjIGSFwQSeeZzUqcaKUQSoyroWzJNxQkZVAEPddsubNGFkdLJ0cqVt1u51JIzSHziklQRDdIXxKpWN2lhl0IVT00TFw7IPVP
+    googleClientID:process.env.GOOGLE_CLIENT_ID, 
+    googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey:process.env.COOKIE_KEY,
+    stripePublishableKey:process.env.STRIPE_PUBLISHABLE_KEY, 
+    stripeSecretKey:process.env.STRIPE_SECRET_KEY
 }
